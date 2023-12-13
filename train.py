234d175d8e896bff0cc19f4b528860af01690bab
LucaCatalano13 <luca.catalano1307@gmail.com>
2023-12-13 11:49:47 +0100

    test not present for little test

--- train.py
+++ train.py
@@ -32,18 +32,18 @@ if __name__ == "__main__":
         train_bound = train_land_cover.get_bound()
         train_dataset = PixelTimeSeries(num_timesteps=5, collection_dataset=train_collection_dataset, bound=train_bound)
         
-    if args.input_test_path is not None:
-        test_dataset = PixelTimeSeries(num_timesteps=5, input_data_path = args.input_test_path)
-    else:
-        test_era = Era5(dataset_folder = args.era5_test_path, legend_folder = args.era5_legend_path)
-        test_dem = Dem(dataset_folder = args.dem_test_path, legend_folder = args.dem_legend_path)
-        test_sentinel3 = Sentinel3(dataset_folder = args.sentinel3_test_path, legend_folder = args.sentinel3_legend_path)
-        test_sentinel5 = Sentinel5(dataset_folder = args.sentinel5_test_path, legend_folder = args.sentinel5_legend_path)
-        test_land_cover = LandCover(dataset_folder = args.land_cover_test_path, legend_folder = args.land_cover_legend_path)
-        test_collection_dataset = CollectionDataset(era = test_era, dem = test_dem, sentinel3 = test_sentinel3, 
-                                                     sentinel5 = test_sentinel5, test_cover = test_land_cover)
-        test_bound = test_land_cover.get_bound()
-        test_dataset = PixelTimeSeries(num_timesteps=5, collection_dataset=test_collection_dataset, bound=test_bound)
+    # if args.input_test_path is not None:
+    #     test_dataset = PixelTimeSeries(num_timesteps=5, input_data_path = args.input_test_path)
+    # else:
+    #     test_era = Era5(dataset_folder = args.era5_test_path, legend_folder = args.era5_legend_path)
+    #     test_dem = Dem(dataset_folder = args.dem_test_path, legend_folder = args.dem_legend_path)
+    #     test_sentinel3 = Sentinel3(dataset_folder = args.sentinel3_test_path, legend_folder = args.sentinel3_legend_path)
+    #     test_sentinel5 = Sentinel5(dataset_folder = args.sentinel5_test_path, legend_folder = args.sentinel5_legend_path)
+    #     test_land_cover = LandCover(dataset_folder = args.land_cover_test_path, legend_folder = args.land_cover_legend_path)
+    #     test_collection_dataset = CollectionDataset(era = test_era, dem = test_dem, sentinel3 = test_sentinel3, 
+    #                                                  sentinel5 = test_sentinel5, test_cover = test_land_cover)
+    #     test_bound = test_land_cover.get_bound()
+    #     test_dataset = PixelTimeSeries(num_timesteps=5, collection_dataset=test_collection_dataset, bound=test_bound)
     
     # train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [int(len(train_dataset)*0.8), int(len(train_dataset)*0.2)])
     
