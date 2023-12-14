76ddc421ebd63b158f59819184b4532358a87e28
LucaCatalano13 <luca.catalano1307@gmail.com>
2023-12-14 12:13:40 +0100

    Update train.py

--- train.py
+++ train.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     # Instantiate a trainer
     trainer = pl.Trainer(
         accelerator='cpu',
-        devices=[0],
+        # devices=[0],
         default_root_dir='./LOGS',  # Tensorflow can be used to viz
         num_sanity_val_steps=0,  # runs a validation step before stating training
         precision=16,  # we use half precision to reduce  memory usage
