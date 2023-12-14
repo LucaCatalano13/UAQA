bf03cdeb855276ad7b9e978e1298455e5ac9f122
LucaCatalano13 <luca.catalano1307@gmail.com>
2023-12-14 12:32:32 +0100

    little data

--- presto/presto.py
+++ presto/presto.py
@@ -423,12 +423,7 @@ class Encoder(nn.Module):
             # and as values a linear combination (FC) from the number of bands in the channel_group
             # to a space of dimension(embedding_size)
             # return an initial embedding of the channel group
-            print("Layer", self.eo_patch_embed[channel_group])
-            print("Channel Group", channel_group)
-            print("Channel Idxs", channel_idxs)
-            print("X", x[:, :, channel_idxs].shape)
             tokens = self.eo_patch_embed[channel_group](x[:, :, channel_idxs])
-            print("Token", tokens.shape)
             # create an embedding of the channel group --> lookup table
             channel_embedding = self.channel_embed(
                 torch.tensor(self.band_group_to_idx[channel_group]).long().to(device)
