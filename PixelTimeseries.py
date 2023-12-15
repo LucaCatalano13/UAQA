ca4afb59652dff7a91f44361666ce9440bfe2d02
jackyjack00 <rossogiacomo2000@gmail.com>
2023-12-15 22:56:56 +0100

    Pixel time sereis now have sliding window

--- PixelTimeseries.py
+++ PixelTimeseries.py
@@ -12,16 +12,20 @@ from datasets.CollectionDataset import CollectionDataset
 from datasets.LandCover import LandCover
 
 class PixelTimeSeries(Dataset):
-    def __init__(self, num_timesteps: int, collection_dataset: CollectionDataset = None, bound: LandCover = None, input_data_path: str = None):
+    def __init__(self, num_timesteps: int, jump = None, collection_dataset: CollectionDataset = None, bound: LandCover = None, input_data_path: str = None):
         super().__init__()
         self.num_timesteps = num_timesteps
+        if jump is not None:
+            self.jump = jump
+        else:
+            self.jump = num_timesteps
         
         if input_data_path is None:
             self.__create_data(collection_dataset, bound)
         else:
             self.__load_data(input_data_path)
-        # till = 500
-        # self.data = (self.data[0][:till], self.data[1][:till], self.data[2][:till], self.data[3][:till])
+         
+        self.num_slices_per_pixel = int( 1 + np.floor( (self.len_pixel_timeseries() - self.num_timesteps)/self.jump ))
 
     def __create_data(self, collection_dataset: CollectionDataset, bound: LandCover): 
         self.data =  process_images(collection_dataset, bound)
@@ -38,16 +42,18 @@ class PixelTimeSeries(Dataset):
 
     def __len__(self):
         # returns number of pixels (FINAL_H * FINAL_W) * N_GIORNI/TIMESTEPS
-        return ((self.data[0].shape[1] * self.data[0].shape[2]) * (self.len_pixel_timeseries() // self.num_timesteps)) - self.num_timesteps
+        return (self.data[0].shape[1] * self.data[0].shape[2]) * self.num_slices_per_pixel
     
     def __getitem__(self, index):
-        t_index = index % (self.len_pixel_timeseries() // self.num_timesteps)
-        start_t = t_index * self.num_timesteps
+        t_index = index % self.num_slices_per_pixel
+        start_t = t_index * self.jump
         #- 1 but excluded by [ start_t : end_t ]
-        end_t = (t_index +1) * self.num_timesteps
-        flat_ix = index // self.len_pixel_timeseries()
+        end_t = start_t + self.num_timesteps
+        
+        flat_ix = index // self.num_slices_per_pixel
         row_ix = flat_ix // FINAL_W
-        col_ix = flat_ix % FINAL_H
+        col_ix = flat_ix % FINAL_W
+        
         arrays = self.data[0][start_t:end_t, row_ix, col_ix, :]
         hard_mask = self.data[1][start_t:end_t, row_ix, col_ix, :]
         latlons = self.data[2][start_t:end_t, row_ix, col_ix, :]
@@ -57,13 +63,23 @@ class PixelTimeSeries(Dataset):
         return arrays, hard_mask, latlons, day_of_year, day_of_week
     
     def retrieve_internal_indices(self, index):
-        t_index = index % (self.len_pixel_timeseries() // self.num_timesteps)
-        start_t = t_index * self.num_timesteps
-        end_t = (t_index + 1) * self.num_timesteps
-        flat_ix = index // self.len_pixel_timeseries()
+        t_index = index % self.num_slices_per_pixel
+        start_t = t_index * self.jump
+        end_t = start_t + self.num_timesteps
+        
+        flat_ix = index // self.num_slices_per_pixel
         row_ix = flat_ix // FINAL_W
-        col_ix = flat_ix % FINAL_H
-        return start_t, end_t - 1, row_ix, col_ix
+        col_ix = flat_ix % FINAL_W
+        res = {
+            "index" : index, 
+            "time index" : t_index, 
+            "start time index" : start_t, 
+            "end time index" : end_t - 1,
+            "flat index" : flat_ix, 
+            "row index" : row_ix, 
+            "column index" : col_ix
+        }
+        return res
         
     # input modello:
     # (BS, TUTTIGIORNI, 1, CHANNEL) --> BS * tuttigiorni/timesteps  (timesteps , 1 , channel)
