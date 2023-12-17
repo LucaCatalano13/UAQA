2bf786e90fc39a1c447619914a235ded40ee46d9
jackyjack00 <128752975+jackyjack00@users.noreply.github.com>
2023-12-17 12:31:13 +0100

    TODO

--- parser.py
+++ parser.py
@@ -6,7 +6,10 @@ def parse_arguments():
     # Training parameters
     parser.add_argument("--batch_size", type=int, default=128,
                         help="batch size")
-     
+    
+    parser.add_argument("--num_timesteps", type=int, default=7,
+                        help="number of days for each training slice of pixel timeseries")
+    
     parser.add_argument("--mask_ratio", type=float, default=0.2,
                         help="mask ratio for soft mask")
     
