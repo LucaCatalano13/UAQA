7d56613758af26d8dc942f946e52e43cae94f8b1
LucaCatalano13 <luca.catalano1307@gmail.com>
2023-12-18 15:15:56 +0100

    Update presto.py

--- presto/presto.py
+++ presto/presto.py
@@ -678,7 +678,6 @@ class Presto(Seq2Seq):
         day_of_year: Union[torch.Tensor, int] = 0,
         day_of_week: Union[torch.Tensor, int] = 0
     ) -> torch.Tensor:
-        print("x nan:" , x.isnan().sum())
         encoded_x = self.encoder(
             x=x,
             latlons=latlons,
