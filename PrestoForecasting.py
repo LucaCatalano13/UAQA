1d163edf6e78219f1b466ad3cfbf32cc134b6c73
LucaCatalano13 <luca.catalano1307@gmail.com>
2023-12-26 19:10:29 +0100

    Update PrestoForecasting.py

--- PrestoForecasting.py
+++ PrestoForecasting.py
@@ -65,7 +65,7 @@ class PrestoForecasting(pl.LightningModule):
         return nn.MSELoss(reduction='none')
 
     def loss_function(self, outputs, y_true, loss_factor):
-        return torch.sum(loss_factor * (outputs - y_true) ** 2)
+        return torch.sum(loss_factor * (outputs - y_true) ** 2) / torch.sum(loss_factor)
     
     def forward(self, x, latlons, hard_mask = None, day_of_year = 0, day_of_week = 0):        
         x = self.encoder(x = x, mask = hard_mask, latlons = latlons, 
