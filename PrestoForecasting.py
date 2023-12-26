bd214fb0d9775199b80e1fffc5132af58d01738c
LucaCatalano13 <luca.catalano1307@gmail.com>
2023-12-26 18:02:35 +0100

    Update PrestoForecasting.py

--- PrestoForecasting.py
+++ PrestoForecasting.py
@@ -65,15 +65,11 @@ class PrestoForecasting(pl.LightningModule):
         return nn.MSELoss()
 
     def loss_function(self, outputs, y_true, loss_factor):
-        loss_factor = torch.Tensor(loss_factor)
-        loss = 0
-        loss = torch.Tensor( loss )
-        
+        loss_factor = torch.Tensor(loss_factor)        
+        print(outputs.shape, y_true.shape, loss_factor.shape)
         #weighted loss on loss_factor := for_each_i [loss_fact_i*loss_i] / for_each_i sum[loss_factor_i]
-        for i, t in enumerate(zip(outputs, y_true)):
-            y_pred, label = t
-            loss += loss_factor[i] * self.loss_fn(y_pred, label)
-        loss = loss/loss_factor.sum()
+        loss = loss_factor * self.loss_fn(outputs, y_true)
+        loss = loss/loss_factor.sum(axis=1)
         return loss
     
     def forward(self, x, latlons, hard_mask = None, day_of_year = 0, day_of_week = 0):        
