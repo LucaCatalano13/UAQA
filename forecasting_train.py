61b2f27298250158b0feb8665fc6054b65a15b43
LucaCatalano13 <luca.catalano1307@gmail.com>
2023-12-29 15:38:37 +0100

    load_from_checkpoint fiixed

--- forecasting_train.py
+++ forecasting_train.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     #Load or initialize a Forecastinf model based on Presto Encoder
     if args.model_presto_forecasting_path is not None:
         #Checkpoint init
-        presto_forecasting = PrestoForecasting.load_from_checkpoint(args.model_presto_forecasting_path, **kwargs_model_forecasting)
+        presto_forecasting = PrestoForecasting.load_from_checkpoint(args.model_presto_forecasting_path)
     else:
         #Random Xavier initialization
         presto_forecasting = PrestoForecasting(**kwargs_model_forecasting)
